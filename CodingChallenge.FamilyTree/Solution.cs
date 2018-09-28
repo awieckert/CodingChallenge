3065065ade4cad22e7e488080c121438675a90a0
awieckert <wiec1369@gmail.com>
2018-09-27 21:09:45 -0500

    Have the basis of what i think will work. Need to start testing the FamilyTree

--- CodingChallenge.FamilyTree/Solution.cs
+++ CodingChallenge.FamilyTree/Solution.cs
@@ -6,7 +6,43 @@ namespace CodingChallenge.FamilyTree
     {
         public string GetBirthMonth(Person person, string descendantName)
         {
+            string theBirthday = "";
+
+            foreach (Person peep in person.Descendants)
+            {
+                if (peep.Name == descendantName)
+                {
+                    theBirthday = peep.Birthday.ToString();
+                } else if (peep.Descendants.Count > 0)
+                {
+                    theBirthday = this.KeepLooking(peep, descendantName);
+                }
+            }
+
+            return theBirthday;
+             
             throw new NotImplementedException();
+        }
+
+        public string KeepLooking (Person person, string descendantName)
+        {
+            string toReturn = "";
+
+            foreach (Person man in person.Descendants)
+            {
+                if (man.Name == descendantName)
+                {
+                    toReturn = man.Birthday.ToString();
+                } else if (man.Descendants.Count > 0)
+                {
+                    this.KeepLooking(man, descendantName);
+                } else
+                {
+                    return toReturn;
+                }
+            }
+
+            return toReturn;            
         }
     }
 }
